37b1f3b6019e4b4a18c6678be04de1b757f620cc
Rupam Das <rupam1624@gmail.com>
2024-07-04 13:54:35 +0530

    yyyy

--- app/community/page.js
+++ app/community/page.js
@@ -3,10 +3,46 @@ import React from 'react'
 const Community = () => {
   return (
     <>
-    <div className="container">
-      <div className="sidebar">sidebar</div>
-      <div className="postbar">Posts</div>
-    </div>
+      <div className="container">
+        <div className="sidebar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Portfolio</li>
+            <li>Careers</li>
+          </ul>
+
+        </div>
+        <div className="postbar">
+          <div className="apost">
+            <div className="clientbox">
+              Rupam Das
+            </div>
+            <div className="order">
+              ok
+            </div>
+          </div>
+          <div className="apost">
+            <div className="clientbox">
+              Rupam Das
+            </div>
+            <div className="order">
+              ok
+            </div>
+          </div>
+          <div className="apost">
+            <div className="clientbox">
+              Rupam Das
+            </div>
+            <div className="order">
+              ok
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
