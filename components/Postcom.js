0f9ce4bf43e610c96122da4fe71907f252651d9f
Rupam Das <rupam1624@gmail.com>
2024-07-06 16:14:48 +0530

    ok

--- components/Postcom.js
+++ components/Postcom.js
@@ -2,13 +2,14 @@
 import React from 'react'
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDebounceValue } from 'usehooks-ts';
 
 const Postcom = () => {
     const [username, setUsername] = useState('');
     const [usernameMessage, setUsernameMessage] = useState('');
     const [isCheckingUsername, setIsCheckingUsername] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
-    
+    const debouncedUsername = useDebounceValue(username,300);
     return (
         <div className="apost">
             <div className="clientbox">
